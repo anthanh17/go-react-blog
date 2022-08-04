95588d7d9401eba0b438e1e58fcf06a004ed0e7c
NamHoaiNguyen <nguyen.hoai.nam.3912@gmail.com>
2022-08-04 23:28:51 +0700

    fix indentation

--- pkg/db/models/account.go
+++ pkg/db/models/account.go
@@ -7,17 +7,17 @@ import (
 )
 
 type Account struct {
-	Id      	uint64			`gorm:"primaryKey; AUTO_INCREMENT"`
-	Username	string			`gorm:"type:varchar(255); not null; unique"`
-	Password 	string			`gorm:"type:varchar(255); not null"`
-	FullName	string			`gorm:"type:varchar(255); not null"`
-	Email		string			`gorm:"type:varchar(255)"`
-	Telephone	string			`gorm:"type:varchar(255)"`
-	LastLogin	time.Time		`json:"created_at"`
+	Id      	uint64       	`gorm:"primaryKey; AUTO_INCREMENT"`
+	Username	string       	`gorm:"type:varchar(255); not null; unique"`
+	Password 	string       	`gorm:"type:varchar(255); not null"`
+	FullName	string       	`gorm:"type:varchar(255); not null"`
+	Email		string       	`gorm:"type:varchar(255)"`
+	Telephone	string       	`gorm:"type:varchar(255)"`
+	LastLogin	time.Time    	`json:"created_at"`
 	Permission	bool
-	Profile		string			`gorm:"type:text"`
-	Subscribe	bool			`gorm:"not null"`
+	Profile		string       	`gorm:"type:text"`
+	Subscribe	bool         	`gorm:"not null"`
 	gorm.Model
-	Post		[]Post			`gorm:"foreignKey:AuthorId"`
+	Post		[]Post      	`gorm:"foreignKey:AuthorId"`
 	PostComment	[]PostComment	`gorm:"foreignKey:AccountCommentId"`
 }
--- pkg/db/models/post_comment.go
+++ pkg/db/models/post_comment.go
@@ -5,11 +5,11 @@ import (
 )
 
 type PostComment struct {
-	Id      			uint64	`gorm:"primaryKey; AUTO_INCREMENT"`
+	Id              	uint64	`gorm:"primaryKey; AUTO_INCREMENT"`
 	ParentCommentId		uint64	`gorm:"not null"`
 	AccountCommentId	uint64	`gorm:"not null"`	
-	PostId				string	`gorm:"type:varchar(255); not null; unique"`
-	Content 			string	`gorm:"type:text"`
-	Mask				bool
+	PostId          	string	`gorm:"type:varchar(255); not null; unique"`
+	Content         	string	`gorm:"type:text"`
+	Mask            	bool
 	gorm.Model
 }
